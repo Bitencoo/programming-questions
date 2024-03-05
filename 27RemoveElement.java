c6876959d2c618c7122e2b56069cd934cc639507
Túlio Bitencourt <81566955+Bitencoo@users.noreply.github.com>
2024-03-04 22:44:43 -0300

    Update 27RemoveElement.java

--- 27RemoveElement.java
+++ 27RemoveElement.java
@@ -12,7 +12,7 @@ class Solution {
 
         int left = 0;
         int right = nums.length - 1;
-        while(left != right) {
+        while(left < right) {
             if(nums[left] == -1) {
                 if(nums[right] != -1) {
                     nums[left] = nums[right];
